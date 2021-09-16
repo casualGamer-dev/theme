8bd1fafa8d017eb5d1ce851a82f0d6e6ebf86b53
casualGamer-dev <niladridas218@gmail.com>
2021-09-16 19:14:55 +0530

    push

--- MasterThemes/build.sh
+++ MasterThemes/build.sh
@@ -1,47 +1,171 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -f "artisan" ]; then
-    echo "Could not find the Artisan file, Moving to Default Location."
-    cd /var/www/pterodactyl
+set -e
+
+########################################################
+# 
+#         Pterodactyl-AutoThemes Installation
+#
+#         Created and maintained by Ferks-FK
+#
+#            Protected by GPL 3.0 License
+#
+########################################################
+
+#### Variables ####
+SCRIPT_VERSION="v0.8.1"
+SUPPORT_LINK="https://discord.gg/buDBbSGJmQ"
+
+
+print_brake() {
+  for ((n = 0; n < $1; n++)); do
+    echo -n "#"
+  done
+  echo ""
+}
+
+
+hyperlink() {
+  echo -e "\e]8;;${1}\a${1}\e]8;;\a"
+}
+
+
+#### Colors ####
+
+GREEN="\e[0;92m"
+YELLOW="\033[1;33m"
+reset="\e[0m"
+
+
+#### OS check ####
+
+check_distro() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$(echo "$ID")
+    OS_VER=$VERSION_ID
+  elif type lsb_release >/dev/null 2>&1; then
+    OS=$(lsb_release -si)
+    OS_VER=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$(echo "$DISTRIB_ID")
+    OS_VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    OS="debian"
+    OS_VER=$(cat /etc/debian_version)
+  elif [ -f /etc/SuSe-release ]; then
+    OS="SuSE"
+    OS_VER="?"
+  elif [ -f /etc/redhat-release ]; then
+    OS="Red Hat/CentOS"
+    OS_VER="?"
+  else
+    OS=$(uname -s)
+    OS_VER=$(uname -r)
+  fi
+
+  OS=$(echo "$OS")
+  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+}
+
+
+#### Install Dependencies ####
+
+dependencies() {
+echo
+print_brake 30
+echo -e "* ${GREEN}Installing dependencies...${reset}"
+print_brake 30
+echo
+case "$OS" in
+debian | ubuntu)
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs && apt-get install -y zip
+;;
+
+centos)
+[ "$OS_VER_MAJOR" == "7" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo yum install -y nodejs yarn && sudo yum install -y zip
+[ "$OS_VER_MAJOR" == "8" ] && curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - && sudo dnf install -y nodejs yarn && sudo dnf install -y zip
+;;
+esac
+}
+
+
+#### Panel Backup ####
+backup() {
+echo
+print_brake 32
+echo -e "* ${GREEN}Performing security backup...${reset}"
+print_brake 32
+if [ -f "/var/www/pterodactyl/PanelBackup/PanelBackup.zip" ]; then
+echo
+print_brake 45
+echo -e "* ${GREEN}There is already a backup, skipping step...${reset}"
+print_brake 45
+echo
+else
+cd /var/www/pterodactyl
+mkdir -p PanelBackup
+zip -r PanelBackup.zip app config public resources routes storage database .env tailwind.config.js
+mv PanelBackup.zip PanelBackup
 fi
+}
 
-if [ ! -f "artisan" ]; then
-    echo "We tried to find your Artisan file but we couldnt, Please move to the directory you installed the Panel and re-run this script. Have a Good Day!"
-    cd /var/www/pterodactyl
-    else
 
-    echo "Your Artisan File has been found!"
-    sleep 2
+#### Donwload Files ####
+download_files() {
+print_brake 25
+echo -e "* ${GREEN}Downloading files...${reset}"
+print_brake 25
+cd /var/www/pterodactyl/resources/scripts
+curl -o user.css https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Dracula/user.css
+rm -R index.tsx
+curl -o index.tsx https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Dracula/index.tsx
+cd
+cd /var/www/pterodactyl/resources/views/layouts
+rm -R admin.blade.php
+curl -o admin.blade.php https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Dracula/admin.blade.php
+cd
+cd /var/www/pterodactyl/public/themes/pterodactyl/css
+curl -o admin.css https://raw.githubusercontent.com/Ferks-FK/Pterodactyl-AutoThemes/${SCRIPT_VERSION}/themes/version1.x/Dracula/admin.css
+cd
+}
 
-    echo "Checking you have ZIP Installed"
-    yum install zip -y 2> /dev/null
-    apt install zip -y 2> /dev/null
+#### Panel Production ####
 
-    echo "Backing up previous panel files in the case that something goes wrong!"
-    zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
+production() {
+DIR=/var/www/pterodactyl
 
-    echo "Downloading the Theme you picked"
-     mkdir -p tempdown && cd tempdown && git clone https://github.com/casualGamer-dev/theme.git .
-    cp -r MasterThemes/public ..
-    cp -r MasterThemes/resources ..
-    cp -r MasterThemes/database ..
-    cp -r MasterThemes/config ..
-    cp -r MasterThemes/app ..
-    cp -r MasterThemes/bootstrap ..
-    cp -r MasterThemes/storage ..    
-    cp -r MasterThemes/tests ..
-    cp -r MasterThemes/routes ..
-    echo "Files have been copied over!"
-    sleep 2
-
-    echo "Removing the temp folders created in the copy process"
-
-    cd .. && rm -rf tempdown
-    
-    echo "Cleaning cache"
-
-    php artisan view:clear
-    php artisan cache:clear
-
-    echo "Complete! Have a good day and dont forget to refresh your browser cache! (CTRL + F5)"
+if [ -d "$DIR" ]; then
+echo
+print_brake 25
+echo -e "* ${GREEN}Producing panel...${reset}"
+print_brake 25
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
 fi
+}
+
+
+bye() {
+print_brake 50
+echo
+echo -e "* ${GREEN}The theme ${YELLOW}gae${GREEN} was successfully installed."
+echo -e "* A security backup of your panel has been created."
+echo -e "* Thank you for using this script."
+
+echo
+print_brake 50
+}
+
+
+#### Exec Script ####
+check_distro
+dependencies
+backup
+download_files
+production
+bye
